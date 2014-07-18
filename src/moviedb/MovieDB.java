0c93c5a0dc0f000bd53b402ff0feefda41efba9e
skiwi <frank_skiwi@hotmail.com>
2014-07-18 22:50:45 +0200

    Initial commit.

---
+++ src/moviedb/MovieDB.java
@@ -0,0 +1,30 @@
+
+package moviedb;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Frank van Heeswijk
+ */
+public class MovieDB extends Application {
+    @Override
+    public void start(final Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MovieDB.fxml"));
+        
+        Scene scene = new Scene(root, 800, 600);
+        
+        primaryStage.setTitle("MovieDB");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
